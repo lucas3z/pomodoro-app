85d3c48fa25dd8430916817b90e78edca699225f
Lucas <lucaoliveira57@gmail.com>
2021-03-12 23:52:43 -0300

    Added react-router-dom & set active className to current counter option

--- src/components/DefineCounter/index.js
+++ src/components/DefineCounter/index.js
@@ -1,22 +1,26 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import NavButton from '../NavButton';
 
 import './style.css';
 
 function DefineCounter({ change }) {
   return (
-    <nav>
-      <ul>
-        <li className='active'>
-          <NavButton text='pomodoro' change={() => change(25)} />
-        </li>
-        <li>
-          <NavButton text='short break' change={() => change(5)} />
-        </li>
-        <li>
-          <NavButton text='long break' change={() => change(10)} />
-        </li>
-      </ul>
-    </nav>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <NavButton text='pomodoro' change={() => change(25)} />
+          </li>
+          <li>
+            <NavButton text='short break' change={() => change(5)} />
+          </li>
+          <li>
+            <NavButton text='long break' change={() => change(10)} />
+          </li>
+        </ul>
+      </nav>
+    </Router>
   );
 }
 
--- src/components/NavButton/index.js
+++ src/components/NavButton/index.js
@@ -1,10 +1,22 @@
+import { Link, useLocation } from 'react-router-dom';
+
 import './style.css';
 
 function NavButton({ text, change }) {
+  const location = useLocation();
+
   return (
-    <button className='nav-btn' onClick={change}>
+    <Link
+      to={text.split(' ')[0]}
+      className={
+        location.pathname === '/' + text.split(' ')[0]
+          ? 'nav-btn active'
+          : 'nav-btn'
+      }
+      onClick={change}
+    >
       {text}
-    </button>
+    </Link>
   );
 }
 
